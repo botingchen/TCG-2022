1ca9ca5f5882cbff0899794b1db4c17c7dc953f4
ptchen1023 <ptchen1023@tcglinux2.cs.nctu.edu.tw>
2022-11-25 03:28:37 +0800

    init pj3

---
+++ Project3/pj-3-code/agent.h
@@ -0,0 +1,181 @@
+/**
+ * Framework for NoGo and similar games (C++ 11)
+ * agent.h: Define the behavior of variants of the player
+ *
+ * Author: Theory of Computer Games
+ *         Computer Games and Intelligence (CGI) Lab, NYCU, Taiwan
+ *         https://cgilab.nctu.edu.tw/
+ */
+
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+#include <fstream>
+#include "board.h"
+#include "action.h"
+#include <cstdlib>
+#include <ctime>
+
+class agent {
+public:
+	agent(const std::string& args = "") {
+		std::stringstream ss("name=unknown role=unknown " + args);
+		for (std::string pair; ss >> pair; ) {
+			std::string key = pair.substr(0, pair.find('='));
+			std::string value = pair.substr(pair.find('=') + 1);
+			meta[key] = { value };
+		}
+	}
+	virtual ~agent() {}
+	virtual void open_episode(const std::string& flag = "") {}
+	virtual void close_episode(const std::string& flag = "") {}
+	virtual action take_action(const board& b) { return action(); }
+	virtual bool check_for_win(const board& b) { return false; }
+
+public:
+	virtual std::string property(const std::string& key) const { return meta.at(key); }
+	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+	virtual std::string name() const { return property("name"); }
+	virtual std::string role() const { return property("role"); }
+
+protected:
+	typedef std::string key;
+	struct value {
+		std::string value;
+		operator std::string() const { return value; }
+		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+		operator numeric() const { return numeric(std::stod(value)); }
+	};
+	std::map<key, value> meta;
+};
+
+/**
+ * base agent for agents with randomness
+ */
+class random_agent : public agent {
+public:
+	random_agent(const std::string& args = "") : agent(args) {
+		if (meta.find("seed") != meta.end())
+			engine.seed(int(meta["seed"]));
+	}
+	virtual ~random_agent() {}
+
+protected:
+	std::default_random_engine engine;
+};
+
+/**
+ * random player for both side
+ * put a legal piece randomly
+ */
+class player : public random_agent {
+public:
+	player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
+		space(board::size_x * board::size_y), who(board::empty) {
+		if (name().find_first_of("[]():; ") != std::string::npos)
+			throw std::invalid_argument("invalid name: " + name());
+		if (role() == "black") who = board::black;
+		if (role() == "white") who = board::white;
+		if (who == board::empty)
+			throw std::invalid_argument("invalid role: " + role());
+		for (size_t i = 0; i < space.size(); i++)
+			space[i] = action::place(i, who);
+	}
+
+	virtual action take_action(const board& state) {
+		std::shuffle(space.begin(), space.end(), engine);
+		for (const action::place& move : space) {
+			board after = state;
+			if (move.apply(after) == board::legal)
+				return move;
+		}
+		return action();
+	}
+
+private:
+	std::vector<action::place> space;
+	board::piece_type who;
+};
+
+struct node{
+	int total_game = 0;
+	int win_game = 0;
+	node* parent = NULL;
+	std::vector<node*> children;
+	board state;
+};
+
+class MCTS_player : public random_agent {
+public:
+	MCTS_player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
+		space(board::size_x * board::size_y), who(board::empty) ,root(new node){
+		if (name().find_first_of("[]():; ") != std::string::npos)
+			throw std::invalid_argument("invalid name: " + name());
+		if (role() == "black") who = board::black;
+		if (role() == "white") who = board::white;
+		if (who == board::empty)
+			throw std::invalid_argument("invalid role: " + role());
+		for (size_t i = 0; i < space.size(); i++)
+			space[i] = action::place(i, who);
+	}
+
+	float UCT(node* cur){				//count for UCT 
+		float win_rate = (float) cur->win_game / (float) cur->total_game;
+		const float c = sqrt(2);
+		float exploitation = sqrt(log(cur->parent->total_game) / cur->total_game);
+		float uct = win_rate + c * exploitation;
+
+		return uct; 
+	}
+
+	node* selection(){					//select which child node
+		node* cur = root;
+		while(cur->children.size() != 0){
+			float best_uct = -1;
+			node* best_child = NULL;
+
+			for(node* child : cur->children){
+				float uct = UCT(child);
+				if(uct > best_uct){
+					uct = best_uct;
+					best_child = child;
+				}
+			}
+
+			cur = best_child;
+		}
+
+		return cur;
+	}
+	void expand(node* cur) {
+
+		for (const action::place& move : space) {
+			board after = cur->state;
+			if (move.apply(after) == board::legal){
+				node* child = new node;
+				child->state = after;
+				cur->children.push_back(child);
+			}
+		}
+
+		return;
+	}
+	virtual action take_action(const board& state) {
+		std::shuffle(space.begin(), space.end(), engine);
+		for (const action::place& move : space) {
+			board after = state;
+			if (move.apply(after) == board::legal)
+				return move;
+		}
+		return action();
+	}
+
+private:
+	std::vector<action::place> space;
+	board::piece_type who;
+	node* root;
+};
